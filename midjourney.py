8af47069ffb94016c590ec06bf2c56c442a7e253
mouxan <mouxan@163.com>
2023-06-09 17:16:44 +0800

    2

--- midjourney.py
+++ midjourney.py
@@ -1,17 +1,15 @@
 # encoding:utf-8
 
+import io
 import json
 import os
 
-import socket
-import web
-from requests_toolbelt import sessions
-
+import webuiapi
+import langid
+from bridge.bridge import Bridge
 import plugins
 from bridge.context import ContextType
 from bridge.reply import Reply, ReplyType
-from channel.chat_message import ChatMessage
-from common import const
 from common.log import logger
 from config import conf
 from plugins import *
@@ -21,56 +19,107 @@ from plugins import *
 class midjourney(Plugin):
     def __init__(self):
         super().__init__()
-        self.handlers[Event.ON_HANDLE_CONTEXT] = self.on_handle_context
-        logger.info("[Hello] inited")
-
-    def on_handle_context(self, e_context: EventContext):
-        if e_context["context"].type not in [
-            ContextType.TEXT,
-            ContextType.JOIN_GROUP,
-            ContextType.PATPAT,
-        ]:
-            return
-
-        if e_context["context"].type == ContextType.JOIN_GROUP:
-            e_context["context"].type = ContextType.TEXT
-            msg: ChatMessage = e_context["context"]["msg"]
-            e_context["context"].content = f'请你随机使用一种风格说一句问候语来欢迎新用户"{msg.actual_user_nickname}"加入群聊。'
-            e_context.action = EventAction.CONTINUE  # 事件继续，交付给下个插件或默认逻辑
-            return
-
-        if e_context["context"].type == ContextType.PATPAT:
-            e_context["context"].type = ContextType.TEXT
-            msg: ChatMessage = e_context["context"]["msg"]
-            e_context["context"].content = f"请你随机使用一种风格介绍你自己，并告诉用户输入#help可以查看帮助信息。"
-            e_context.action = EventAction.CONTINUE  # 事件继续，交付给下个插件或默认逻辑
-            return
-
-        content = e_context["context"].content
-        logger.debug("[Hello] on_handle_context. content: %s" % content)
-        if content == "Hello":
-            reply = Reply()
-            reply.type = ReplyType.TEXT
-            msg: ChatMessage = e_context["context"]["msg"]
-            if e_context["context"]["isgroup"]:
-                reply.content = f"Hello, {msg.actual_user_nickname} from {msg.from_user_nickname}"
+        curdir = os.path.dirname(__file__)
+        config_path = os.path.join(curdir, "config.json")
+        try:
+            with open(config_path, "r", encoding="utf-8") as f:
+                config = json.load(f)
+                self.rules = config["rules"]
+                defaults = config["defaults"]
+                self.default_params = defaults["params"]
+                self.default_options = defaults["options"]
+                self.start_args = config["start"]
+                self.api = webuiapi.WebUIApi(**self.start_args)
+            self.handlers[Event.ON_HANDLE_CONTEXT] = self.on_handle_context
+            logger.info("[SD] inited")
+        except Exception as e:
+            if isinstance(e, FileNotFoundError):
+                logger.warn(f"[SD] init failed, {config_path} not found, ignore or see https://github.com/zhayujie/chatgpt-on-wechat/tree/master/plugins/sdwebui .")
             else:
-                reply.content = f"Hello, {msg.from_user_nickname}"
-            e_context["reply"] = reply
-            e_context.action = EventAction.BREAK_PASS  # 事件结束，并跳过处理context的默认逻辑
+                logger.warn("[SD] init failed, ignore or see https://github.com/zhayujie/chatgpt-on-wechat/tree/master/plugins/sdwebui .")
+            raise e
+    
+    def on_handle_context(self, e_context: EventContext):
 
-        if content == "Hi":
-            reply = Reply()
-            reply.type = ReplyType.TEXT
-            reply.content = "Hi"
-            e_context["reply"] = reply
-            e_context.action = EventAction.BREAK  # 事件结束，进入默认处理逻辑，一般会覆写reply
+        if e_context['context'].type != ContextType.IMAGE_CREATE:
+            return
+        channel = e_context['channel']
+        if ReplyType.IMAGE in channel.NOT_SUPPORT_REPLYTYPE:
+            return
 
-        if content == "End":
-            # 如果是文本消息"End"，将请求转换成"IMAGE_CREATE"，并将content设置为"The World"
-            e_context["context"].type = ContextType.IMAGE_CREATE
-            content = "The World"
+        logger.debug("[SD] on_handle_context. content: %s" %e_context['context'].content)
+
+        logger.info("[SD] image_query={}".format(e_context['context'].content))
+        reply = Reply()
+        try:
+            content = e_context['context'].content[:]
+            # 解析用户输入 如"横版 高清 二次元:cat"
+            if ":" in content:
+                keywords, prompt = content.split(":", 1)
+            else:
+                keywords = content
+                prompt = ""
+
+            keywords = keywords.split()
+            unused_keywords = []
+            if "help" in keywords or "帮助" in keywords:
+                reply.type = ReplyType.INFO
+                reply.content = self.get_help_text(verbose = True)
+            else:
+                rule_params = {}
+                rule_options = {}
+                for keyword in keywords:
+                    matched = False
+                    for rule in self.rules:
+                        if keyword in rule["keywords"]:
+                            for key in rule["params"]:
+                                rule_params[key] = rule["params"][key]
+                            if "options" in rule:
+                                for key in rule["options"]:
+                                    rule_options[key] = rule["options"][key]
+                            matched = True
+                            break  # 一个关键词只匹配一个规则
+                    if not matched:
+                        unused_keywords.append(keyword)
+                        logger.info("[SD] keyword not matched: %s" % keyword)
+                
+                params = {**self.default_params, **rule_params}
+                options = {**self.default_options, **rule_options}
+                params["prompt"] = params.get("prompt", "")
+                if unused_keywords:
+                    if prompt:
+                        prompt += f", {', '.join(unused_keywords)}"
+                    else:
+                        prompt = ', '.join(unused_keywords)
+                if prompt:
+                    lang = langid.classify(prompt)[0]
+                    if lang != "en":
+                        logger.info("[SD] translate prompt from {} to en".format(lang))
+                        try:
+                            prompt = Bridge().fetch_translate(prompt, to_lang= "en")
+                        except Exception as e:
+                            logger.info("[SD] translate failed: {}".format(e))
+                        logger.info("[SD] translated prompt={}".format(prompt))
+                    params["prompt"] += f", {prompt}"
+                if len(options) > 0:
+                    logger.info("[SD] cover options={}".format(options))
+                    self.api.set_options(options)
+                logger.info("[SD] params={}".format(params))
+                result = self.api.txt2img(
+                    **params
+                )
+                reply.type = ReplyType.IMAGE
+                b_img = io.BytesIO()
+                result.image.save(b_img, format="PNG")
+                reply.content = b_img
+            e_context.action = EventAction.BREAK_PASS  # 事件结束后，跳过处理context的默认逻辑
+        except Exception as e:
+            reply.type = ReplyType.ERROR
+            reply.content = "[SD] "+str(e)
+            logger.error("[SD] exception: %s" % e)
             e_context.action = EventAction.CONTINUE  # 事件继续，交付给下个插件或默认逻辑
+        finally:
+            e_context['reply'] = reply
 
     def get_help_text(self, **kwargs):
         help_text = "欢迎使用MJ机器人\n"
