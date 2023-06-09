87bc6e375f32c40fc0cd2413292c09fbe504cd55
mouxan <mouxan@163.com>
2023-06-09 17:21:07 +0800

    2

--- midjourney.py
+++ midjourney.py
@@ -1,12 +1,5 @@
 # encoding:utf-8
 
-import json
-import os
-
-import socket
-import web
-from requests_toolbelt import sessions
-
 import plugins
 from bridge.context import ContextType
 from bridge.reply import Reply, ReplyType
@@ -72,23 +65,5 @@ class midjourney(Plugin):
             e_context.action = EventAction.CONTINUE  # 事件继续，交付给下个插件或默认逻辑
 
     def get_help_text(self, **kwargs):
-        help_text = "欢迎使用MJ机器人\n"
-        help_text += f"这是一个AI绘画工具，只要输入想到的文字，通过人工智能产出相对应的图。\n"
-        help_text += f"------------------------------\n"
-        help_text += f"🎨 AI绘图-使用说明：\n"
-        help_text += f"输入: /imagine prompt\n"
-        help_text += f"prompt 即你提的绘画需求\n"
-        help_text += f"------------------------------\n"
-        help_text += f"📕 prompt附加参数 \n"
-        help_text += f"1.解释: 在prompt后携带的参数, 可以使你的绘画更别具一格\n"
-        help_text += f"2.示例: /imagine prompt --ar 16:9\n"
-        help_text += f"3.使用: 需要使用--key value, key和value空格隔开, 多个附加参数空格隔开\n"
-        help_text += f"------------------------------\n"
-        help_text += f"📗 附加参数列表\n"
-        help_text += f"1. --v 版本 1,2,3,4,5 默认5, 不可与niji同用\n"
-        help_text += f"2. --niji 卡通版本 空或5 默认空, 不可与v同用\n"
-        help_text += f"3. --ar 横纵比 n:n 默认1:1\n"
-        help_text += f"4. --q 清晰度 .25 .5 1 2 分别代表: 一般,清晰,高清,超高清,默认1\n"
-        help_text += f"5. --style 风格 (4a,4b,4c)v4可用 (expressive,cute)niji5可用\n"
-        help_text += f"6. --s 风格化 1-1000 (625-60000)v3"
+        help_text = "输入Hello，我会回复你的名字\n输入End，我会回复你世界的图片\n"
         return help_text
