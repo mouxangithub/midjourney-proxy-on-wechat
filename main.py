4363d0e219fd3fe6b4e22e5f8e862293cdb6445d
mouxan <mouxan@163.com>
2023-06-26 17:20:33 +0800

    3

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # encoding:utf-8
-import plugins
+import time
+import requests
 from bridge.context import ContextType
 from bridge.reply import Reply, ReplyType
 from common.log import logger
+import plugins
 from plugins import *
-from .api import mj
 
 @plugins.register(
     name="midjourney",
@@ -24,6 +25,7 @@ class MidJourney(Plugin):
         ]:
             return
         
+        mj = _mjApi()
         channel = e_context['channel']
         context = e_context['context']
         content = context.content
@@ -67,4 +69,147 @@ class MidJourney(Plugin):
 
 
     def get_help_text(self, **kwargs):
+        mj = _mjApi()
         return mj.help_text()
+
+
+
+class _mjApi():
+    def __init__(self):
+        self.baseUrl = "http://mj.mouxan.cn"
+        self.headers = {
+            "mj-api-secret": "mouxan",
+            "Content-Type": "application/json",
+        }
+    
+    def imagine(self, text):
+        try:
+            url = self.baseUrl + "/mj/submit/imagine"
+            data = {"prompt": text}
+            res = requests.post(url, json=data, headers=self.headers)
+            code = res.json()["code"]
+            if code == 1:
+                msg = "✅ 您的任务已提交\n"
+                msg += f"🚀 正在快速处理中，请稍后\n"
+                msg += f"📨 任务ID: {res.json()['result']}\n"
+                msg += f"🪄 查询进度\n"
+                msg += f"✏  使用[/fetch + 任务ID操作]\n"
+                msg += f"/fetch {res.json()['result']}"
+                return True, msg, res.json()["result"]
+            else:
+                return False, res.json()["description"]
+        except Exception as e:
+            return False, "图片生成失败"
+    
+    def simpleChange(self, content):
+        try:
+            url = self.baseUrl + "/mj/submit/simple-change"
+            data = {"content": content}
+            res = requests.post(url, json=data, headers=self.headers)
+            code = res.json()["code"]
+            if code == 1:
+                msg = "✅ 您的任务已提交\n"
+                msg += f"🚀 正在快速处理中，请稍后\n"
+                msg += f"📨 任务ID: {res.json()['result']}\n"
+                msg += f"🪄 查询进度\n"
+                msg += f"✏  使用[/fetch + 任务ID操作]\n"
+                msg += f"/fetch {res.json()['result']}"
+                return True, msg, res.json()["result"]
+            else:
+                return False, res.json()["description"]
+        except Exception as e:
+            return False, "图片生成失败"
+    
+    def fetch(self, id):
+        try:
+            url = self.baseUrl + f"/mj/task/{id}/fetch"
+            res = requests.get(url, headers=self.headers)
+            status = res.json()['status']
+            submitTime = ""
+            startTime = ""
+            finishTime = ""
+            if res.json()['submitTime']:
+                submitTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(res.json()['submitTime']/1000))
+            if res.json()['startTime']:
+                startTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(res.json()['startTime']/1000))
+            if res.json()['finishTime']:
+                finishTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(res.json()['finishTime']/1000))
+            msg = "✅ 查询成功\n"
+            msg += f"任务ID: {res.json()['id']}\n"
+            msg += f"描述内容：{res.json()['prompt']}\n"
+            msg += f"状态：{self.status(status)}\n"
+            msg += f"进度：{res.json()['progress']}\n"
+            if startTime:
+                msg += f"开始时间：{startTime}\n"
+            if submitTime:
+                msg += f"提交时间：{submitTime}\n"
+            if finishTime:
+                msg += f"完成时间：{finishTime}\n"
+            if res.json()['imageUrl']:
+                msg += f"图片地址：{res.json()['imageUrl']}\n"
+            return True, msg
+        except Exception as e:
+            return False, "查询失败"
+    
+    def status(self, status):
+        msg = ""
+        if status == "SUCCESS":
+            msg = "已成功"
+        elif status == "FAILURE":
+            msg = "失败"
+        elif status == "SUBMITTED":
+            msg = "已提交"
+        elif status == "IN_PROGRESS":
+            msg = "处理中"
+        else:
+            msg = "未知"
+        return msg
+    
+    def get_f_img(self, id):
+        try:
+          url = self.baseUrl + f"/mj/task/{id}/fetch"
+          status = ""
+          rj = ""
+          while status != "SUCCESS":
+              time.sleep(3)
+              res = requests.get(url, headers=self.headers)
+              rj = res.json()
+              status = rj["status"]
+          action = rj["action"]
+          msg = ""
+          if action == "IMAGINE":
+              msg = f"🎨 绘图成功\n"
+              msg += f"✨ 内容: {rj['prompt']}\n"
+              msg += f"✨ 内容: {rj['prompt']}\n"
+              msg += f"📨 任务ID: {id}\n"
+              msg += f"🪄 放大 U1～U4，变换 V1～V4\n"
+              msg += f"✏ 使用[/up 任务ID 操作]\n"
+              msg += f"/up {id} U1"
+          elif action == "UPSCALE":
+              msg = "🎨 放大成功\n"
+              msg += f"✨ {rj['description']}\n"
+          return True, msg, rj["imageUrl"]
+        except Exception as e:
+            return False, "绘图失败"
+    
+    def help_text(self):
+        help_text = "欢迎使用MJ机器人\n"
+        help_text += f"这是一个AI绘画工具，只要输入想到的文字，通过人工智能产出相对应的图。\n"
+        help_text += f"------------------------------\n"
+        help_text += f"🎨 AI绘图-使用说明：\n"
+        help_text += f"输入: /mj prompt\n"
+        help_text += f"prompt 即你提的绘画需求\n"
+        help_text += f"------------------------------\n"
+        help_text += f"📕 prompt附加参数 \n"
+        help_text += f"1.解释: 在prompt后携带的参数, 可以使你的绘画更别具一格\n"
+        help_text += f"2.示例: /mj prompt --ar 16:9\n"
+        help_text += f"3.使用: 需要使用--key value, key和value空格隔开, 多个附加参数空格隔开\n"
+        help_text += f"------------------------------\n"
+        help_text += f"📗 附加参数列表\n"
+        help_text += f"1. --v 版本 1,2,3,4,5 默认5, 不可与niji同用\n"
+        help_text += f"2. --niji 卡通版本 空或5 默认空, 不可与v同用\n"
+        help_text += f"3. --ar 横纵比 n:n 默认1:1\n"
+        help_text += f"4. --q 清晰度 .25 .5 1 2 分别代表: 一般,清晰,高清,超高清,默认1\n"
+        help_text += f"5. --style 风格 (4a,4b,4c)v4可用 (expressive,cute)niji5可用\n"
+        help_text += f"6. --s 风格化 1-1000 (625-60000)v3"
+        return help_text
